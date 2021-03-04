b838142f9df766cd8a680a34d9c458aa3267fb14
Quintin Donnelly <quinndonnelly22@gmail.com>
2021-03-04 07:28:16 -0500

    Add base project and 3rd party assets

---
+++ Assets/Scenes/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float jumpVelocity;
+    private Rigidbody2D _rigidbody2D;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = transform.GetComponent<Rigidbody2D>();
+        print(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var velocity = _rigidbody2D.velocity;
+            velocity = new Vector2(velocity.x, velocity.y + jumpVelocity);
+            _rigidbody2D.velocity = velocity;
+        }
+    }
+}
