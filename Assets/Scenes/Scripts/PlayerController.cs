e9b69a21d8b195ced28092f5dcf6342c2e96b608
Quintin Donnelly <quinndonnelly22@gmail.com>
2021-03-05 05:17:30 -0500

    Add inital controls for walking and jumping
    Semi control in the air with full snap movement on the ground
    Moved the hit box of charecter to be a box collider due to the rolling
    affect on the edges of platforms

--- Assets/Scenes/Scripts/PlayerController.cs
+++ Assets/Scenes/Scripts/PlayerController.cs
@@ -7,7 +7,11 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float jumpVelocity;
     private Rigidbody2D _rigidbody2D;
-    
+    [SerializeField] private LayerMask platformLayerMask;
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float airMoveSpeed;
+    [SerializeField] private CapsuleCollider2D groundCheck;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +22,64 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        PlayerControls();
+    }
+
+    private void PlayerControls()
+    {
+        JumpControls();
+        MovementControls();
+    }
+
+    private void MovementControls()
+    {
+        var velocity = _rigidbody2D.velocity;
+        
+        if (Input.GetKey(KeyCode.D))
         {
-            var velocity = _rigidbody2D.velocity;
-            velocity = new Vector2(velocity.x, velocity.y + jumpVelocity);
-            _rigidbody2D.velocity = velocity;
+            if (!IsGrounded())
+            {
+                velocity += new Vector2(+movementSpeed * airMoveSpeed * Time.deltaTime, 0);
+                _rigidbody2D.velocity =
+                    new Vector2(Mathf.Clamp(velocity.x, -movementSpeed, +movementSpeed), velocity.y);
+            }
+            else
+            {
+                _rigidbody2D.velocity = new Vector2(+movementSpeed, velocity.y);   
+            }
         }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            if (!IsGrounded())
+            {
+                velocity += new Vector2(-movementSpeed * airMoveSpeed * Time.deltaTime, 0);
+                _rigidbody2D.velocity =
+                    new Vector2(Mathf.Clamp(velocity.x, -movementSpeed, +movementSpeed), velocity.y);
+            }
+            else
+            {
+                _rigidbody2D.velocity = new Vector2(-movementSpeed, velocity.y);   
+            }
+        }
+        else if (IsGrounded())
+        {
+            _rigidbody2D.velocity = new Vector2(0, velocity.y);
+        }
+    }
+    
+    private void JumpControls()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            _rigidbody2D.velocity = Vector2.up * jumpVelocity;
+        }
+    }
+    
+    private bool IsGrounded()
+    {
+        var position = transform.position;
+        Collider2D overlap = Physics2D.OverlapCircle(groundCheck.bounds.center, groundCheck.size.y, platformLayerMask);
+        // ReSharper disable once Unity.PerformanceCriticalCodeNullComparison
+        return overlap;
     }
 }
